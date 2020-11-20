f194098ebdc8227b487ea037db77d8195ce8f4f1
Hok KimLong <hokkimlong@outlook.com>
2020-11-20 10:26:31 +0700

    secondg

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,20 +5,14 @@ export default function Home() {
   return (
     <Helmet>
       <title>HomePage</title>
-      <meta
-        property="og:title"
-        content="Product Hunt – The best new products in tech."
-      />
+      <meta property="og:title" content="HomePage - Appiwat" />
       <meta property="og:type" content="article" />
       <meta
         property="og:image"
-        content="https://api.url2png.com/v6/P5329C1FA0ECB6/790272390317dc724643b1ca88f5da6e/png/?url=https://www.producthunt.com/"
+        content="https://openthread.google.cn/images/ot-contrib-google.png"
       />
-      <meta
-        property="og:deschttps://local-ogp.firebaseapp.com/site/zVFBfE25WIIIb0eIC8Pyription"
-        content="Product Hunt is a curation of the best new products, every day. Discover the latest mobile apps, websites, and technology products that everyone&#x27;s talking about."
-      />
-      <meta property="og:url" content="https://www.producthunt.com/" />
+      <meta property="og:description" content="Best It Solution" />
+      <meta property="og:url" content="https://www.appiwat.com/" />
     </Helmet>
   )
 }
